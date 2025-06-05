a84f279f85d7d37f0fcdfd0e2da68ec82601d49b
daniel_windows11 <new_pc@widnw.com>
2025-06-05 02:58:07 -0500

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	renamed:    .github/workflows -> .github/workflows/deploy.yml
    	modified:   astro.config.mjs
    :wq

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://lund133369.github.io',
-	base: 'astro_github_pages_ejemplo',
 	integrations: [mdx(), sitemap()],
 });
