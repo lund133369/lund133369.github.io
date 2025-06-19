378d8280439994e214d04c400e89eecbe70dfb3e
daniel_windows11 <new_pc@widnw.com>
2025-06-19 00:38:54 -0500

    Update favicon and site constants for improved branding and content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog-hack lund';
+export const SITE_DESCRIPTION = 'Bienvenido al blog de lund.dev, donde comparto mis conocimientos y experiencias en desarrollo web, programación y tecnología. Aquí encontrarás artículos, tutoriales y recursos útiles para mejorar tus habilidades y mantenerte actualizado en el mundo del desarrollo.';
